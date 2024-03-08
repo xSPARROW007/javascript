ddf5f72e76b9e9fd49ebb9b14fc8f9e6ce73843d
xSPARROW007 <126142725+xSPARROW007@users.noreply.github.com>
2024-03-08 13:53:21 +0000

    data types

---
+++ basic/02_dataType.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3 + 3) // we are using nodejs , not browser
+
+// code readability should be high
+
+let name = "archit"
+let age = 20
+let isLoggedIn = false
+
+// number = 2 to power 53
+// bigint
+// string = ""
+// boolean = true/ false
+// null = standalone value
+// undefined = 
+// symbol = unique 
+
+// object
+
+console.log(typeof "archit");
+console.log(typeof age);
+console.log(typeof null);// object
+console.log(typeof undefined);
